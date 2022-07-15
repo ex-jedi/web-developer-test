795e08e9397f4ccc94f24793d48a4f32413f0154
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-07-15 19:04:49 +0100

    Resetting

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,1 @@
-import * as Flickity from 'flickity';
-import 'flickity-imagesloaded';
-// ********** Flickity **********
-const carousel = document.querySelector('.website-section__carousel');
-const flkty = new Flickity(carousel, {
-  imagesLoaded: true,
-  autoPlay: 1000,
-  prevNextButtons: true,
-  wrapAround: true,
-  pageDots: false,
-  contain: true
-});
+console.log('Hello');
