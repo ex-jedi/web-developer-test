8efd78b726551a6de43a4c7db867c141da773ea9
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-07-16 13:03:12 +0100

    Removing some unused Flickity CSS

--- dist/index.e72c3fb5.js
+++ dist/index.e72c3fb5.js
@@ -1,6 +1,5 @@
 const carousel = document.querySelector(".website-section__carousel");
 const flkty = new Flickity(carousel, {
-    autoPlay: 1000,
     wrapAround: false,
     pageDots: false
 });
--- scripts/main-min.js
+++ scripts/main-min.js
@@ -1,2 +1,2 @@
-!function(){const e=document.querySelector(".website-section__carousel");new Flickity(e,{autoPlay:1e3,wrapAround:!1,pageDots:!1})}();
+!function(){const e=document.querySelector(".website-section__carousel");new Flickity(e,{wrapAround:!1,pageDots:!1})}();
 //# sourceMappingURL=main-min.js.map
