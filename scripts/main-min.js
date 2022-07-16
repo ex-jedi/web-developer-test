28c4bf67dce493176b4bee5a5f6ad3e8de5541be
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-07-16 17:01:04 +0100

    Deleting unused files

--- scripts/main-min.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector(".website-section__carousel"),t=new Flickity(e,{wrapAround:!0,pageDots:!1,prevNextButtons:!1});document.querySelector(".website-section__carousel-buttons--previous").addEventListener("click",(()=>{t.previous()}));document.querySelector(".website-section__carousel-buttons--next").addEventListener("click",(()=>{t.next()}))}();
-//# sourceMappingURL=main-min.js.map
