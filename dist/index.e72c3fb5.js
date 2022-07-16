db5088bc6f412c8ba44112364393850d5e0da676
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-07-16 13:54:39 +0100

    Flickity custom buttons

--- dist/index.e72c3fb5.js
+++ dist/index.e72c3fb5.js
@@ -1,7 +1,17 @@
 const carousel = document.querySelector(".website-section__carousel");
 const flkty = new Flickity(carousel, {
     wrapAround: false,
-    pageDots: false
+    pageDots: false,
+    prevNextButtons: false
+});
+const previousButton = document.querySelector(".website-section__carousel-buttons--previous");
+previousButton.addEventListener("click", ()=>{
+    flkty.previous();
+});
+// next
+const nextButton = document.querySelector(".website-section__carousel-buttons--next");
+nextButton.addEventListener("click", ()=>{
+    flkty.next();
 });
 
 //# sourceMappingURL=index.e72c3fb5.js.map
--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,15 @@ const carousel = document.querySelector('.website-section__carousel');
 const flkty = new Flickity(carousel, {
   wrapAround: false,
   pageDots: false,
+  prevNextButtons: false,
+});
+
+const previousButton = document.querySelector('.website-section__carousel-buttons--previous');
+previousButton.addEventListener('click', () => {
+  flkty.previous();
+});
+// next
+const nextButton = document.querySelector('.website-section__carousel-buttons--next');
+nextButton.addEventListener('click', () => {
+  flkty.next();
 });
